757a662afff69610908009b4180acc0de9a9dcb3
Sangjune Park <60810276+JJukE@users.noreply.github.com>
2023-02-20 18:48:26 +0900

    Delete train_autoencoder.sh

--- train_autoencoder.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 \
-# python train_autoencoder.py --dataset_path /root/data/CLIPForge/ShapeNet/ # voxel
-python train_autoencoder.py --dataset_path /root/data/CLIPForge/ShapeNet/ --input_type Pointcloud --output_type Pointcloud # pointcloud
